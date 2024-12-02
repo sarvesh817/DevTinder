784bb0603e8ac8bb78ac12218320ec0fe03d31e1
Sarvesh <serveshk7@gmail.com>
2024-12-02 18:46:29 +0530

    Schema validation with comment

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@ const express=require("express");
 const app=express();
 
 const {adminAuth}=require("../middlewares/auth");
+const connectDB=require("./config/database");
+const User=require("./models/user"); 
 
-
+app.use(express.json());
 
 //Note - for apply all admin routes  
-//app.use("/hello",adminAuth);  
+//app.use("/hello",adminAuth);   
 app.use("/hello/2",(req,res,next)=>{
   //res.send("hello 2");           
-  next();
+  next();  
 },
 (req,res)=>{
     res.send("this is the part - route handler");               
@@ -21,7 +23,7 @@ app.use("/hello",adminAuth,(req,res)=>{
     res.send("hello hello hello");       
 }); 
 
-//Error Handlers  by below way && by usig try catch  
+//Error Handlers  by below way for handle each and every all error so && by usig try catch  
 app.use("/",(err,req,res,next)=>{
     if(err){  
         res.status(500).send("Something went wrong");
@@ -32,17 +34,84 @@ app.use("/",(err,req,res,next)=>{
  
 
 //if hit requrest like /xyz then below code still works bcz -wildcard slach so   
-app.use("/",(req,res)=>{   
+/* app.use("/",(req,res)=>{   
     res.send("slash routes ");      
-});    
+});    */ 
 
 
-
-
-app.listen(3000,() => {
-    console.log("server is running on the port 3000");     
+connectDB()
+.then(() => {
+    console.log("DB connected successfully");
+    app.listen(3000,() => {
+        console.log("server is running on the port 3000");     
+    });
+})
+.catch((error) => {
+    console.log("DB can't connect", error);
 });
- 
+
+
+ //Crud API
+ app.post("/signup",async(req,res)=>{
+    try{
+        //Creating a new instance of userModel
+        const user=new User(req.body);
+        const userData=await user.save();
+        if(userData){   
+            res.status(200).json({message:"UserData Insertion Successfully ",userData});   
+        }else{
+            res.status(400).json({message:"userData insertion Failed"});   
+        }
+        
+    }catch(error){
+        res.status(400).send("Error - "+error.message);    
+
+    }
+ });
+
+// Fetch User Records
+app.get("/getUser", async (req, res) => {
+    try {
+        const getUser = await User.find();
+        res.status(200).json({message: "Users information fetched successfully",users: getUser});
+    } catch (error) {
+        res.status(500).json({message: "Error occurred while fetching users",error: error.message});
+    }
+});
+
+
+
+//Update User Record
+app.patch("/updUser",async(req,res)=>{
+    try {
+        const userId=req.body.userId;
+        const data=req.body; 
+        const updUser=await User.findByIdAndUpdate({_id:userId},data,{
+              returnDocument:"after",
+              runValidators:true,  
+        });         
+        if(updUser)
+            res.status(200).json({message: "Users Updated successfully",users: updUser});
+        else
+            res.status(500).json({message: "Users Updation Failed",users: updUser});       
+    } catch (error) {
+        res.status(500).json({message: "Error occurred while fetching users",error: error.message});
+    }
+});
+
+//Delete User Record  
+app.delete("/delUser",async(req,res)=>{
+    try {
+        const userId=req.body.userId;
+        const delUser=await User.findByIdAndDelete(userId);    
+        if(delUser)
+            res.status(200).json({message: "Users Deleted successfully",users: delUser});
+        else
+            res.status(500).json({message: "Users Deletion Failed",users: delUser});    
+    } catch (error) {
+        res.status(500).json({message: "Error occurred while fetching users",error: error.message});
+    }
+});
 
 //part 2 - Advanced routing - ?,+,*,(bc)group  or regex
 //Dynamic routing  - :  e.g /user/:userID and getting userID by req.params  //for get userID      
---
+++ src/models/user.js
@@ -0,0 +1,56 @@
+const mongoose=require("mongoose");
+
+const userSchema=new mongoose.Schema({
+    firstName:{
+        type:String,
+        //for required field
+        required:true,
+        minLength:3
+    },
+    lastName:{
+        type:String,
+        required:true 
+    },
+    emailId:{
+        type:String,
+        //for lowercase
+        lowercase:true,  
+        required:true,
+        unique:true, 
+        //for remove white space  otherwise same email id will enter with space then it will treated as uniq email id so always use trim. 
+        trim:true    
+    },
+    password:{ 
+        type:String  
+    },
+    age:{
+        type:Number 
+    },
+    gender:{
+        type:String,
+        //this is custom validation way---
+        validate(value){
+            if(!['male','female','others'].includes(value)){
+                throw new Error("Gender is not valid");        
+            }
+        }
+    },
+    about:{
+        type:String,
+        //for default value pass 
+        default:"This is a default value." 
+    },
+    skills:{
+        //you can add multiple skills now in form of Array. 
+        type:[String]
+    }  
+}, 
+{ 
+    //by write below timestamps true - now mongodb will enter each document with created and updated data time bydefault automatically.   
+    timestamps:true }
+);      
+
+
+const User=mongoose.model("User",userSchema);
+module.exports=User;  
+  
